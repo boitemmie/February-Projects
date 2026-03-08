e989731ff87f7c86e0663d9dc23919472ee09fad
Godwin Temmie <godwintemmie@gmail.com>
2026-03-08 06:53:17 +0100

    Added Second week projects

---
+++ 2ND WEEK PROJECT/hello.js
@@ -0,0 +1,33 @@
+// 1. 1. Write a function that returns a personalized message e.g "Hello Temmie".
+
+function greet(name) {
+  return "hello " + name;
+}
+
+console.log(greet("Temmie"));
+
+// 2. Write a function that adds, squares, coverts minutes to seconds
+
+// 2a.
+
+function add(a, b) {
+  return a + b;
+}
+
+consolo.log(add(2, 5));
+
+// 2b.
+
+function square(a) {
+  return a ** 2;
+}
+
+console.log(square(10));
+
+// 2c.
+
+function minutesToSeconds(a) {
+  return a * 60;
+}
+
+console.log(minutesToSeconds(5));
